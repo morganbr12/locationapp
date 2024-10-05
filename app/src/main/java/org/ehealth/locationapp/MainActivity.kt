0f7894742de28647206fab252b14bca6491f63f2
fdankwah <fredrick@appsnmobilesolutions.com>
2024-10-05 01:00:41 +0000

    first commit empty project

--- app/src/main/java/org/ehealth/locationapp/MainActivity.kt
+++ app/src/main/java/org/ehealth/locationapp/MainActivity.kt
@@ -1,16 +1,27 @@
 package org.ehealth.locationapp
 
+import android.content.Context
 import android.os.Bundle
+import android.Manifest
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import org.ehealth.locationapp.ui.theme.LocationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +30,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LocationAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MyApp();
                 }
             }
         }
@@ -31,17 +39,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    val context = LocalContext.current;
+    val locationUtil = LocationUtil(context);
+    LocationDisplay(locationUtil = locationUtil, context = context)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LocationAppTheme {
-        Greeting("Android")
+fun LocationDisplay(
+    locationUtil: LocationUtil,
+    context: Context,
+) {
+
+    val requestPermissionsLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permission ->
+            if (permission[Manifest.permission.ACCESS_COARSE_LOCATION] == true && permission[Manifest.permission.ACCESS_COARSE_LOCATION] == true) {
+//                permission granted
+            } else {
+//                ask for permission
+                val rationalRequired = ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                )
+
+                if (rationalRequired) {
+                    Toast.makeText(
+                        context,
+                        "Location permission is required for this feature to work",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(context, "Location permission is required Please allow it in your settings", Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = "This is the Address");
+
+        Button(onClick = {
+            if (locationUtil.hasLocationPermission(context)) {
+//                location has permission Update location address
+            } else {
+//                Request location permission
+                requestPermissionsLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                    )
+                )
+            }
+        }) {
+            Text(text = "Get Location");
+        }
     }
 }
